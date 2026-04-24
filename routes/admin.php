978bf9c421f9cde02f759aab1715123a30d8ef9e
Alfredo Palacios <joseaph.1998@gmail.com>
2026-04-24 14:11:36 -0600

    Move route definitions from `web.php` to `admin.php` under domain grouping for better organization.

--- routes/admin.php
+++ routes/admin.php
@@ -2,5 +2,32 @@
 
 
 use App\Http\Controllers\ReceptionController;
+use Inertia\Inertia;
+
+Route::redirect('/', '/dashboard');
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->name('dashboard');
+
+Route::get('/tables', function () {
+    return Inertia::render('Tables');
+})->name('tables');
+
+Route::get('/forms', function () {
+    return Inertia::render('Forms');
+})->name('forms');
+
+Route::get('/charts', function () {
+    return Inertia::render('Charts');
+})->name('charts');
+
+Route::get('/components', function () {
+    return Inertia::render('Components');
+})->name('components');
+
+Route::get('/settings', function () {
+    return Inertia::render('Settings');
+})->name('settings');
 
 Route::get('reception', [ReceptionController::class, 'index']);
