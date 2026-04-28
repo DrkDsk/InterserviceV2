b94252b58adc3fd2f98aea4067dd12575bd54c17
Alfredo Palacios <joseaph.1998@gmail.com>
2026-04-27 18:45:51 -0600

    Bind `ClientRepositoryInterface` to `ClientRepository` in `RepositoryProvider` for client data management

--- app/Providers/RepositoryProvider.php
+++ app/Providers/RepositoryProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Repositories\Contract\ClientRepositoryInterface;
 use App\Repositories\Contract\DeviceCategoryRepositoryInterface;
 use App\Repositories\Contract\UserRepositoryInterface;
+use App\Repositories\Eloquent\ClientRepository;
 use App\Repositories\Eloquent\DeviceCategoryRepository;
 use App\Repositories\Eloquent\UserRepository;
 use Illuminate\Support\ServiceProvider;
@@ -17,6 +19,7 @@ class RepositoryProvider extends ServiceProvider
     {
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
         $this->app->bind(DeviceCategoryRepositoryInterface::class, DEviceCategoryRepository::class);
+        $this->app->bind(ClientRepositoryInterface::class, ClientRepository::class);
     }
 
     /**
