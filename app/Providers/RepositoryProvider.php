a2c63c9ad111158508a7882f23b300466bd25e55
Alfredo Palacios <joseaph.1998@gmail.com>
2026-04-28 11:17:12 -0600

    feat: interface binded

--- app/Providers/RepositoryProvider.php
+++ app/Providers/RepositoryProvider.php
@@ -4,9 +4,15 @@ namespace App\Providers;
 
 use App\Repositories\Contract\ClientRepositoryInterface;
 use App\Repositories\Contract\DeviceCategoryRepositoryInterface;
+use App\Repositories\Contract\DeviceRepositoryInterface;
+use App\Repositories\Contract\ReceptionRepositoryInterface;
+use App\Repositories\Contract\RepairRepositoryInterface;
 use App\Repositories\Contract\UserRepositoryInterface;
 use App\Repositories\Eloquent\ClientRepository;
 use App\Repositories\Eloquent\DeviceCategoryRepository;
+use App\Repositories\Eloquent\DeviceRepository;
+use App\Repositories\Eloquent\ReceptionRepository;
+use App\Repositories\Eloquent\RepairRepository;
 use App\Repositories\Eloquent\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +26,9 @@ class RepositoryProvider extends ServiceProvider
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
         $this->app->bind(DeviceCategoryRepositoryInterface::class, DEviceCategoryRepository::class);
         $this->app->bind(ClientRepositoryInterface::class, ClientRepository::class);
+        $this->app->bind(RepairRepositoryInterface::class, RepairRepository::class);
+        $this->app->bind(DeviceRepositoryInterface::class, DeviceRepository::class);
+        $this->app->bind(ReceptionRepositoryInterface::class, ReceptionRepository::class);
     }
 
     /**
