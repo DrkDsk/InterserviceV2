1948f8118bc75599839130a086a1db57e6760b39
Alfredo Palacios <joseaph.1998@gmail.com>
2026-04-27 19:19:32 -0600

    Add Spanish validation messages in `lang/es/validation.php`

---
+++ lang/es/validation.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'required' => 'El campo :attribute es obligatorio',
+    'string' => 'El campo debe ser una cadena',
+    'email' => 'El campo debe ser una correo electrónico',
+    'min' => 'El campo :attribute debe tener mínimo :min caracteres',
+    'max' => 'El campo :attribute debe tener máximo :max caracteres',
+    'confirmed' => 'El campo :attribute no coincide',
+    'unique' => 'El campo :attribute ya está ocupado',
+    'exist' => 'El campo :attribute no está registrado',
+];
