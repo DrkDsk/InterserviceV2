8ba33829887de4e529eabfe043440461f9f0fb4b
Alfredo Palacios <joseaph.1998@gmail.com>
2026-04-28 16:54:09 -0600

    Add `status` shared prop to Inertia middleware for session status retrieval

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -25,6 +25,7 @@ class HandleInertiaRequests extends Middleware
                 'success' => fn () => $request->session()->get('success'),
                 'error' => fn () => $request->session()->get('error'),
             ],
+            'status' => fn () => $request->session()->get('status'),
         ]);
     }
 }
