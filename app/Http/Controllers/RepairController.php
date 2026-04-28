a045cca0493d1f65b7fb5c5e0c520fd9ac5ed44f
Alfredo Palacios <joseaph.1998@gmail.com>
2026-04-27 18:46:03 -0600

    Inject `GetClientsUseCase` into `RepairController` and pass fetched clients to `RepairCreate` view

--- app/Http/Controllers/RepairController.php
+++ app/Http/Controllers/RepairController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\useCases\DeviceCategories\GetDeviceCategoriesUseCase;
+use App\useCases\Client\GetClientsUseCase;
+use App\useCases\DeviceCategory\GetDeviceCategoriesUseCase;
 use Inertia\Inertia;
 
 class RepairController extends Controller
@@ -12,12 +13,14 @@ class RepairController extends Controller
         return Inertia::render('Repair/RepairIndex');
     }
 
-    public function create(GetDeviceCategoriesUseCase $getDeviceCategoriesUseCase)
+    public function create(GetDeviceCategoriesUseCase $getDeviceCategoriesUseCase, GetClientsUseCase $getClientsUseCase)
     {
         $deviceCategories = $getDeviceCategoriesUseCase->execute();
+        $clients = $getClientsUseCase->execute();
 
         return Inertia::render('Repair/RepairCreate', [
-            'deviceCategories' => $deviceCategories
+            'deviceCategories' => $deviceCategories,
+            'clients' => $clients,
         ]);
     }
 
