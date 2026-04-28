c07e781c3ef7b32fe948d9f8ecb19d9b28cd0844
Alfredo Palacios <joseaph.1998@gmail.com>
2026-04-28 14:44:45 -0600

    Enable additional seeders in `DatabaseSeeder` for Services and Categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            /*UserSeeder::class,*/
+            UserSeeder::class,
             ClientSeeder::class,
             DeviceCategorySeeder::class,
+            ServiceCategorySeeder::class,
+            ServiceSeeder::class,
         ]);
     }
 }
