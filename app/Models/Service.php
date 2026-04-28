37d53fe1e9592699782300dc0d18510fe3dff303
Alfredo Palacios <joseaph.1998@gmail.com>
2026-04-28 14:30:45 -0600

    Add `Service` model with fillable attributes for service data

---
+++ app/Models/Service.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Service extends Model
+{
+    protected $fillable = [
+        'service_category_id',
+        'name',
+        'price',
+        'requires_diagnosis',
+        'requires_visit',
+    ];
+}
