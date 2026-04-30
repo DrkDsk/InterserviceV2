16f4a3b175d9ff5366cea7aa812247a2e746bed6
Alfredo Palacios <joseaph.1998@gmail.com>
2026-04-29 19:40:51 -0600

    Add `limit` and `paginate` methods to `BaseRepository` and `BaseRepositoryInterface` for enhanced query control.

--- app/Repositories/Contract/BaseRepositoryInterface.php
+++ app/Repositories/Contract/BaseRepositoryInterface.php
@@ -4,15 +4,20 @@ namespace App\Repositories\Contract;
 
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 interface BaseRepositoryInterface
 {
     public function getAll(): Collection;
+
+    public function limit(int $limit = 100): Collection;
 
     public function find(int $id): ?Model;
 
     public function getByField(string $field, string $value): ?Model;
 
     public function create(array $data): ?Model;
+
+    public function paginate(int $perPage = 10): LengthAwarePaginator;
 }
 
