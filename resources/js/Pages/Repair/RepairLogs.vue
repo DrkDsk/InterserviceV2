1cbd2c9465aeb9f11b2576642740fc51bffaf687
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-04 22:47:44 -0600

    Refactor: Rename `updateLog` to `upsertLog` and improve log handling in `RepairLogs.vue`

--- resources/js/Pages/Repair/RepairLogs.vue
+++ resources/js/Pages/Repair/RepairLogs.vue
@@ -30,13 +30,15 @@ const addLog = () => {
   })
 }
 
-const updateLog = (id, message) => {
+const upsertLog = (id, message) => {
   if (id == null) {
     router.post(route("repairs.logs.store", props.repair.id), {
       message
     })
   } else {
-    router.put(route("repairs.logs.update", id))
+    router.put(route("repairs.logs.update", id), {
+      message
+    })
   }
 }
 
@@ -57,7 +59,7 @@ const updateLog = (id, message) => {
                 <span class="text-xs font-semibold text-slate-500 dark:text-slate-400">{{ index + 1 }}</span>
                 <AppTextarea class="w-10/12" v-model="log.message" :rows="1"/>
                 <AppIcon @click="removeLog(log.id)" class="text-red-400 cursor-pointer" name="fa-trash"/>
-                <AppIcon @click="updateLog(log.id, log.message)" class="text-green-200 cursor-pointer"
+                <AppIcon @click="upsertLog(log.id, log.message)" class="text-green-200 cursor-pointer"
                          name="fa-circle-check"/>
               </div>
             </div>
