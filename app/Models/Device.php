bbdafa64bbaae497f61f35695a23a6762fc77182
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-04 19:49:10 -0600

    Add `deviceCategory` relationship to `Device` model

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Device extends Model
 {
@@ -18,4 +19,9 @@ class Device extends Model
         'observations',
         'accessories',
     ];
+
+    public function deviceCategory(): BelongsTo
+    {
+        return $this->belongsTo(DeviceCategory::class);
+    }
 }
