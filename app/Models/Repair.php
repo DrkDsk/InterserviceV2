38ccc2b91770380cebca68937e312276bb49accd
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-04 19:49:06 -0600

    Add `logs` relationship to `Repair` model for associated repair logs

--- app/Models/Repair.php
+++ app/Models/Repair.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Repair extends Model
 {
@@ -38,5 +39,10 @@ class Repair extends Model
     public function service(): BelongsTo
     {
         return $this->belongsTo(Service::class);
+    }
+
+    public function logs(): HasMany
+    {
+        return $this->hasMany(RepairLog::class);
     }
 }
